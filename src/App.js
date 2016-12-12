c086b185d0cef5d2e0fdd138c6c106cd600ecb77
Bradley Cravens <bradleycravens@gmail.com>
2016-12-12 12:00:09 -0500

    Bradley Cravens Password Validator

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      password: '',
+      confirm: ''
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleConfirmChange = this.handleConfirmChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value});
+  }
+
+  handleConfirmChange(event) {
+    this.setState({confirm: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.password === this.state.confirm) {
+      alert("Login successful");
+    } else {
+      alert("Login failed: Please make sure passwords match");
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label> Email: </label>
+        <br />
+        <input type="text" value={this.state.name} onChange={this.handleNameChange} />
+        <br />
+        <label> Password: </label>
+        <br />
+        <input type="text" value={this.state.password} onChange={this.handlePasswordChange} />
+        <br />
+        <label> Confirm Password: </label>
+        <br />
+        <input type="text" value={this.state.confirm} onChange={this.handleConfirmChange} />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
 
-export default App;
+export default Login;
